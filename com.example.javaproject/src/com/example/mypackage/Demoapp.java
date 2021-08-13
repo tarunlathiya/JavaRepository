684c389877c43ec64cbd5617d5f2a0b042c45c5a
tarunlathiya <tlathiya@logilite.com>
2021-08-13 11:01:52 +0530

    Initial First commit

---
+++ com.example.javaproject/src/com/example/mypackage/Demoapp.java
@@ -0,0 +1,25 @@
+package com.example.mypackage;
+
+import java.util.Scanner;
+
+public class Demoapp
+{
+
+	public static void main(String[] args)
+	{
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String s = sc.next();
+		System.out.println("Hello " + s);
+
+		Runnable runnable = new Runnable() {
+
+			@Override
+			public void run()
+			{
+				System.out.println("anonymous class called");
+			}
+		};
+	}
+}
